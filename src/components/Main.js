37e23344c506c7ff5e83d376324b62a02aa26703
Anastasiya Esakova <nastya77esakova@yandex.ru>
2021-11-13 16:43:05 +0300

    fix: Main, api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PopupWithForm from './PopupWithForm';
 import Card from './Card';
-import api from '../utils/api';
+import api from '../utils/api.js';
 import ImagePopup from './ImagePopup';
 
 function  Main(props){
