77bba672574c4a7bbba51240627e103d4e1610b4
Anastasiya Esakova <nastya77esakova@yandex.ru>
2022-04-26 00:25:55 +0300

    feat: base header

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -44,10 +44,10 @@ function EditProfilePopup(props){
   return(
     <PopupWithForm title="Редактировать профиль" name="profile" onClose={props.onClose} isOpen={props.isOpen} buttonText="Сохранить"
     onSubmit={handleSubmit} isLoading={props.isLoading} handleOverlayClose={props.handleOverlayClose} formValid={formValid}>
-      <input className={`popup__input popup__input_el_name ${nameValid ? "" : "popup__input_type_error"}`} id ="name-profile" name="name" type="text" value={name}
+      <input className={`popup__input popup__input_el_name ${nameValid ? "" : "popup__input_type_error"}`} id ="name-profile" name="name" type="text" value={name||''}
       placeholder="Имя" minLength="2" maxLength="40" required onChange={handleChangeName}/>
       <span className={`popup__error ${nameValid ? "": "popup__error_visible"}`} id="name-profile-error">{errorNameMessage}</span>
-      <input className={`popup__input popup__input_el_job ${descriptionValid ? "" : "popup__input_type_error"}`} id="job-profile" type="text" value={description}
+      <input className={`popup__input popup__input_el_job ${descriptionValid ? "" : "popup__input_type_error"}`} id="job-profile" type="text" value={description||''}
        placeholder="Описание" name="about" minLength="2" maxLength="200" required onChange={handleChangeDescription}/>
       <span className={`popup__error ${descriptionValid ? "": "popup__error_visible"}`} id="job-profile-error">{errorDescriptionMessage}</span>
     </PopupWithForm>
